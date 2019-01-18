2690e482ce38558a3afb6bc3b520a55fcacdc5da
Toktam  Rezaei <toktamrezaei23@gmail.com>
2019-01-18 14:19:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const startingPoint = 42
+const feetInBlock = 264
+
+// 43 -> 1
+function distanceFromHqInBlocks(block) {
+  if (block < startingPoint) {
+    return startingPoint - block
+  } else {
+    return block - startingPoint
+  }
+}
+
+function distanceFromHqInFeet(block) {
+  if (block < startingPoint) {
+    return (startingPoint - block) * feetInBlock
+  } else {
+    return (block - startingPoint) * feetInBlock
+  }
+  
+  function distanceTravelledInFeet(startingBlock, endingBlock) {
+    if (startingBlock < endingBlock) {
+      
+      
+    }
+  }
